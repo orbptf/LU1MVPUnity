9251d9823b5552af994ecef6920fde4294b67524
Sietse Versluis <sj.versluis1@student.avans.nl>
2025-03-12 20:35:47 +0100

    FINAL UNITY VERSION
    changed the way objects are getting deleted

--- LU1 MVP Sietse Versluis GIT/Assets/Scripts/WorldDeletionHandler.cs
+++ LU1 MVP Sietse Versluis GIT/Assets/Scripts/WorldDeletionHandler.cs
@@ -58,9 +58,9 @@ public class WorldDeletionHandler : MonoBehaviour
 
     
 
-    public async void ClickedConfirmDeletion()
+    public void ClickedConfirmDeletion()
     {
-        await DeleteSelectedWorldObjects();
+        //await DeleteSelectedWorldObjects();
         DeleteSelectedWorld();
     }
 
